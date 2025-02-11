4532994bf2cd343b346d43226127657140b7c7eb
olamapsteam <admin@olamaps.io>
2025-02-11 08:33:11 +0000

    Update distribution

---
+++ dist/index.d.ts
@@ -0,0 +1,36 @@
+import { GeolocateControlOptions } from 'maplibre-gl';
+import { Map as Map_2 } from 'maplibre-gl';
+import { MapOptions } from 'maplibre-gl';
+import { MarkerOptions } from 'maplibre-gl';
+import { NavigationControlOptions } from 'maplibre-gl';
+import * as OlaMapsGl from 'maplibre-gl';
+import { PopupOptions } from 'maplibre-gl';
+
+declare interface olaMapProps {
+    apiKey: string;
+    mode?: string;
+    threedTileset?: string;
+    accessToken?: string;
+}
+
+export declare class OlaMaps {
+    private olaMaps;
+    private apiKey;
+    private accessToken;
+    mode: string;
+    private threedTileset;
+    constructor({ apiKey, accessToken, mode, threedTileset }: olaMapProps);
+    private addOlaLogo;
+    private addAttribution;
+    private fetchStaticMap;
+    private add3dLayer;
+    init(options?: MapOptions): Map_2 & typeof OlaMapsGl;
+    addNavigationControls(options?: NavigationControlOptions): OlaMapsGl.NavigationControl;
+    addGeolocateControls(options: GeolocateControlOptions): OlaMapsGl.GeolocateControl;
+    getMercatorCoordinate(): typeof OlaMapsGl.MercatorCoordinate;
+    addMarker(options?: MarkerOptions): OlaMapsGl.Marker;
+    addPopup(options?: PopupOptions): OlaMapsGl.Popup;
+    getStaticMap(url: string, elementID: string): void;
+}
+
+export { }
